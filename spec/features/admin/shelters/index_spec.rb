075b5283178f2b543b795dd6e6b19e58f12205e0
Lisa Taylor <ltaylor.lisa@gmail.com>
2022-02-14 14:05:17 -0700

    Feat: user story #6 complete

--- spec/features/admin/shelters/index_spec.rb
+++ spec/features/admin/shelters/index_spec.rb
@@ -18,4 +18,17 @@ RSpec.describe 'the admin/shelter index' do
       expect(page).to have_content(shelter_1.name)
     end
   end
+
+  it '/admin/shelters index shows link and navigates to show page' do
+    shelter_1 = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: true, rank: 9)
+    shelter_2 = Shelter.create!(name: 'Denver shelter', city: 'Denver, CO', foster_program: true, rank: 9)
+    shelter_3 = Shelter.create!(name: 'Maxfund', city: 'Denver, CO', foster_program: true, rank: 9)
+
+    visit "/admin/shelters"
+
+    within('#shelter_name-0') do
+      click_link("#{shelter_3.name}")
+      expect(current_path).to eq("/admin/shelters/#{shelter_3.id}")
+    end
+  end
 end
